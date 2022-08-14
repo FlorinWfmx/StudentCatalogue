1d74c1511bb791916f4a1e595638830f6a8ff5ab
FlorinWfmx <93483958+FlorinWfmx@users.noreply.github.com>
2022-08-14 17:50:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class CourseMain {
+public class CatalogueMain {
 
     public static void main(String[] args) throws EmptyCatalogueException, IOException {
         Catalogue.printMenu();
